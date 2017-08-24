642fe85778dff06b2de22a16a26f74b2c7212d2e
Shuhei28 <shuhei.112028@gmail.com>
2017-08-25 01:38:03 +0900

    #

--- SPEACE/scenes/next/director.rb
+++ SPEACE/scenes/next/director.rb
@@ -7,7 +7,7 @@ module Next
     def play
       Scene.add_scene(Game::Director.new,  :game)
       Scene.add_scene(Game2::Director.new,  :game2)
-      #Scene.add_scene(Game3::Director.new,  :game3)
+      Scene.add_scene(Game3::Director.new,  :game3)
       Scene.set_current_scene(:game) if Input.key_push?(K_A)
       Scene.set_current_scene(:game2) if Input.key_push?(K_B)
       Scene.set_current_scene(:game3) if Input.key_push?(K_C)
